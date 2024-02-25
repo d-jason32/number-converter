8a686968525ae5147db90b909e8607ca5f98a58e
Jason Devaraj <jdevaraj01@gmail.com>
2024-02-25 00:15:38 -0500

    2/25 2/25

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function displayInput() {
     // Get the value from the input field
-    const userInput = document.getElementById('userInput').value;
+    const userInput = document.getElementById('decimal').value;
   
     // Display the input value on the screen
     const outputDiv = document.getElementById('output');
